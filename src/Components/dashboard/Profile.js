2e5f81d116ef0cfdfce4025f699e836b5cafca5e
telio-s <telio.s@hotmail.com>
2021-11-18 15:18:45 +0700

    log profile

--- src/Components/dashboard/Profile.js
+++ src/Components/dashboard/Profile.js
@@ -174,7 +174,19 @@ const Profile = (props) => {
           </>
         )
       ) : (
-        <h1>No infomationnnnnnnnnnnnnnnnnnnnnnnnnnnn</h1>
+        <div class="divLoader">
+          <svg width="5em" height="5em" style={{ paddingLeft: "20px" }}>
+            <circle
+              cx="50"
+              cy="50"
+              fill="none"
+              stroke="#215d38"
+              stroke-width="10"
+              r="35"
+              stroke-dasharray="164.93361431346415 56.97787143782138"
+            ></circle>
+          </svg>
+        </div>
       )}
     </div>
   );
