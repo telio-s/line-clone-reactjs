aaa9d4a66851eb3d02def2fa342bee7a059e0ffc
telio-s <telio.s@hotmail.com>
2021-11-23 17:18:19 +0700

    AWS config

--- src/index.js
+++ src/index.js
@@ -6,19 +6,36 @@ import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
-import AWS from "aws-sdk";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+var AWS = require("aws-sdk");
 Amplify.configure(config);
 console.log("nize tao");
-AWS.config.maxRetries = 20;
+const dynamo = new AWS.DynamoDB({
+  region: "ap-southeast-1",
+  maxRetries: 10,
+  retryDelayOptions: {
+    base: 300,
+  },
+});
+console.log("dynamo", dynamo);
 AWS.events.on("retry", function (resp) {
   console.log("nize", resp);
   // Enable or disable retries completely.
   // disabling is equivalent to setting maxRetries to 0.
-  if (resp.error) resp.error.retryable = true;
+  // if (resp.error) resp.error.retryable = true;
 
-  // retry all requests with a 2sec delay (if they are retryable)
-  if (resp.error) resp.error.retryDelay = 2000;
+  // // retry all requests with a 2sec delay (if they are retryable)
+  // if (resp.error) resp.error.retryDelay = 2000;
+  if (resp.error && resp.error.retryable) {
+    var date = new Date();
+    console.log(
+      date,
+      "| Retrying request for the " + resp.retryCount + "th time."
+    );
+    console.log(date, "| Retry triggered by", resp.error.message);
+  }
 });
+const client = new DynamoDBClient({ region: "ap-southeast-1" });
 
 debugger;
 
